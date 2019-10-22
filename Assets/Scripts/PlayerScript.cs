ae9febee0624aa2e39c92e3c2bca33d27d660ed4
Alex Deane <alexrdeane@hotmail.com>
2019-10-23 09:48:38 +1100

    shrek

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    /*
+    public LayerMask LayerMask;
+    public Camera cam;
+   
+    public void InteractCamera()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, 1f, LayerMask))
+        {
+            
+        }
+    }
+    */
+}
