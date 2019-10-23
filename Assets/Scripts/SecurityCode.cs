30632ce3bef41a6800070afa4b310bf2c93d2d6a
Alex Deane <alexrdeane@hotmail.com>
2019-10-23 11:40:27 +1100

    security code stuff

--- Assets/Scripts/SecurityCode.cs
+++ Assets/Scripts/SecurityCode.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SecurityCode : MonoBehaviour
 {
+    public string[] code;
+    public TMP_Text display;
+    public int codeOutput;
+    public int securityCode;
+    public bool isUnlocked;
     void Start()
     {
         
@@ -11,6 +17,43 @@ public class SecurityCode : MonoBehaviour
 
     void Update()
     {
-        
+        if(code[3] != "*")
+        {
+            codeOutput = int.Parse(display.text);
+            if(codeOutput == securityCode)
+            {
+                isUnlocked = true;
+            }
+            else
+            {
+                for (int i = 0; i < code.Length; i++)
+                {
+                    code[i] = "*";
+                }
+                display.text = code[0] + code[1] + code[2] + code[3];
+
+            }
+        }
     }
+
+    public void Keypad(int value)
+    {
+        Debug.Log(value);
+        for (int i = 0; i < code.Length; i++)
+        {
+            Debug.Log("loop");
+
+            if (code[i].Contains("*"))
+            {
+                Debug.Log("Contains");
+                code[i] = code[i].Replace("*", value.ToString());
+                Debug.Log("Replace");
+                display.text = code[0] + code[1] + code[2] + code[3];
+                return;
+
+            }
+        }
+    }
+
+
 }
