80e9b25959fd18392ab5ac1c66932b3a22a9a127
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2019-10-29 19:49:42 +1100

    Added more folders, moved files to respective folders, added basic shooting mechanic

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 10f;
+    public GameObject effectsPrefab;
+    public Transform line;
+    public AudioSource weaponFx;
+    public AudioClip bulletFx;
+
+    private Rigidbody rigid;
+
+    void Awake()
+    {
+        // Get component on awake so we don't miss it if it starts disabled
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (rigid.velocity.magnitude < 0)
+        {
+            // Rotate the line to face direction of bullet travel
+            line.transform.rotation = Quaternion.LookRotation(rigid.velocity);
+        }
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        // ContactPoint contact = col.contacts[0];
+        // Instantiate(effectsPrefab, contact.point, Quaternion.LookRotation(contact.normal));
+        // Destroy bullet
+        Destroy(gameObject);
+        AudioSource.PlayClipAtPoint(bulletFx, transform.position, 0.5f);
+    }
+
+    public void Fire(Vector3 lineOrigin, Vector3 direction)
+    {
+        // Add an instant force to bullet
+        rigid.AddForce(direction * speed, ForceMode.Impulse);
+        // Set line's origin (different from starting position)
+        line.transform.position = lineOrigin;
+    }
+}
