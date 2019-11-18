13dda98fa6b32ac34f5736b53f0a9f036b1e71d9
Alex Deane <alexrdeane@hotmail.com>
2019-11-18 15:26:08 +1100

    keycode workiing

---
+++ Assets/Scripts/Keypad.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keypad : MonoBehaviour
+{
+    public GameObject securityPanel;
+
+    private void Awake()
+    {
+        securityPanel.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            securityPanel.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            securityPanel.SetActive(false);
+        }
+    }
+}
