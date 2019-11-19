01a67284535eb8741de2605ade1bf03df42be233
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2019-11-20 10:07:29 +1100

    Added new Scene, SceneSwitcher script, sfx folder

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public bool stirlingMode = false;
+
+    public void GotoGameScene()
+    {
+        if (stirlingMode == false)
+        {
+            SceneManager.LoadScene(1);
+        }
+        if (stirlingMode == true)
+        {
+            SceneManager.LoadScene(2);
+        }
+
+    }
+
+    public void GotoTitleScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void GotoStirlingScene()
+    {
+        SceneManager.LoadScene(2);
+        stirlingMode = true;
+    }
+}
