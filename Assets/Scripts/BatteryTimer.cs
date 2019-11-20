7915a096a11851c99b79123a476ba03ac0989e0e
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2019-11-20 14:16:19 +1100

    Updated batteryGauge

---
+++ Assets/Scripts/BatteryTimer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BatteryTimer : MonoBehaviour
+{
+    public Slider batterySlider;
+    public float battery;
+    public float maxBattery;
+    public bool torchOn;
+
+    private void Start()
+    {
+        battery = maxBattery;
+        batterySlider.maxValue = maxBattery;
+        batterySlider.value = battery;
+    }
+
+    private void Update()
+    {
+        battery = Mathf.Clamp(battery,0,maxBattery);
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            torchOn = !torchOn;
+        }
+        if(torchOn == true)
+        {
+            battery -= 0.7f;
+            batterySlider.value = battery;
+        }
+        else if (torchOn == false)
+        {
+            battery += 0.5f;
+            batterySlider.value = battery;
+        }
+
+    }
+}
