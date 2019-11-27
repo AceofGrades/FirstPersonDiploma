89dd626bfab3d591de4b9657dd36a0ea47b0dbdd
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2019-11-27 11:04:11 +1100

    Added pause menu, added buttons which resume, load main menu or quit the game

--- Assets/Scripts/GameManager/PauseMenu.cs
+++ Assets/Scripts/GameManager/PauseMenu.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -56,7 +57,7 @@ public class PauseMenu : MonoBehaviour
 
     public void QuitGame()
     {
-        Debug.Log("Quiting Game...");
+        Debug.Log("Quitting Game...");
         Application.Quit();
     }
 }
