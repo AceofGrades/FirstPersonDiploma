f84f7d01180e21c521041ba70102ccad4d22113b
AceofGrades <47581354+AceofGrades@users.noreply.github.com>
2019-12-02 11:51:45 +1100

    Added radar sounds

---
+++ Assets/Scripts/RadarDistance.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadarDistance : MonoBehaviour
+{
+    AudioSource radar;
+    public AudioClip safeRadar;
+    public AudioClip dangerRadar;
+    
+    void Start()
+    {
+        audio.Play(safeRadar);
+        audio.Stop(dangerRadar);
+    }
+
+    void OnCollisionEnter (Collision col)
+    {
+        if(col.gameObject.tag == "Enemy")
+        {
+            AudioSource.PlayClipAtPoint(dangerRadar);
+            AudioSource.Stop(safeRadar);
+        }
+        else
+        {
+            audio.Play(safeRadar);
+            audio.Stop(dangerRadar);
+        }
+    }
+}
